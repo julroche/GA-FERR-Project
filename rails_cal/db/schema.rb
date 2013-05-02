7074b204f6217ea91a0fa4328ca774db48704a94
Julia Roche <julroche@yahoo.com>
2013-05-01 20:50:50 -0400

    Create user migration in calendar app

--- rails_cal/db/schema.rb
+++ rails_cal/db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418224229) do
+ActiveRecord::Schema.define(:version => 20130502004450) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.datetime "start_at"
     t.datetime "end_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
