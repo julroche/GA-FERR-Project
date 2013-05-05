eea74e3a3d938037e0d70fb7656f3de6b0dcc3bd
Julia Roche <julroche@yahoo.com>
2013-05-05 14:33:48 -0400

    Add options view, updatesign-up and  log-in routes

--- rails_cal/config/routes.rb
+++ rails_cal/config/routes.rb
@@ -7,6 +7,7 @@ CalTest2::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
+  get "options" => "welcome#options", :as => "options"
 
 
 
