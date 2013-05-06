ea889c48ee910a6a5747d547794ec6a4fd33f1ec
Julia Roche <julroche@yahoo.com>
2013-05-05 23:28:50 -0400

    Add activity_id field to Event model and add name to sign-up form

--- rails_cal/app/models/event.rb
+++ rails_cal/app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-  attr_accessible :end_at, :name, :start_at
+  attr_accessible :end_at, :name, :start_at, :activity_id
   has_event_calendar
 end
--- rails_cal/db/schema.rb
+++ rails_cal/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130506005642) do
+ActiveRecord::Schema.define(:version => 20130506031224) do
 
   create_table "activities", :force => true do |t|
     t.string   "activity_name"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(:version => 20130506005642) do
     t.string   "name"
     t.datetime "start_at"
     t.datetime "end_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "activity_id"
   end
 
   create_table "users", :force => true do |t|
