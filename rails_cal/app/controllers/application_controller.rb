96cb9bac3c5702c0ff55805a02b5a02eb55e7ab5
Julia Roche <julroche@yahoo.com>
2013-05-08 00:33:00 -0400

    Update events table with activity name and end time

--- rails_cal/app/controllers/application_controller.rb
+++ rails_cal/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 	helper_method :current_user
+	# helper_method :current_activity
   
   private
   
@@ -8,5 +9,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  #  def current_activity
+  #   @current_activity ||= Activity.find(params[:id])
+  # end
 
 end
