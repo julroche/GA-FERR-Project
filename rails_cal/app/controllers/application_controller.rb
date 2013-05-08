f1afc630db03b75e5aa264c48f7590cfd95b4364
Julia Roche <julroche@yahoo.com>
2013-05-08 12:36:42 -0400

    Create activity instance variable in calendar controller

--- rails_cal/app/controllers/application_controller.rb
+++ rails_cal/app/controllers/application_controller.rb
@@ -2,16 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 	helper_method :current_user
 	helper_method :current_activity
-  
+
   private
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-   def current_activity
-    @current_activity ||= Activity.find(2)
-
-  end
+ 
 
 end
--- rails_cal/app/controllers/calendar_controller.rb
+++ rails_cal/app/controllers/calendar_controller.rb
@@ -1,6 +1,14 @@
 class CalendarController < ApplicationController
   
+  def current_activity
+    @current_activity ||= @activity
+  end
+
   def index
+    @activity = Activity.find(6)
+    # @activity = Activity.find(params[:id])
+  
+
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
 
@@ -8,6 +16,8 @@ class CalendarController < ApplicationController
 
     # @event_strips = Event.event_strips_for_month(@shown_month)
   	@event_strips = current_activity.events.event_strips_for_month(@shown_month)
+  
+
   end
   
 end
