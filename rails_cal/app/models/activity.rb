7436ccc80615e0791469948efdcf5f3e1082ded5
Julia Roche <julroche@yahoo.com>
2013-05-11 11:39:21 -0400

    Add paperclip gem.

--- rails_cal/app/models/activity.rb
+++ rails_cal/app/models/activity.rb
@@ -1,8 +1,10 @@
 class Activity < ActiveRecord::Base
-  attr_accessible :activity_name, :user_id
+  attr_accessible :activity_name, :user_id, :activity_icon
 
   belongs_to :user
   has_many :events
+
+ 	has_attached_file :activity_icon
 
   validates_presence_of :activity_name
   validates_presence_of :user_id
